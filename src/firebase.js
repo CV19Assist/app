5507954fecc3378a8e93ac3e46a52afec027e391
Farhan Ahmad <farhan@thebitguru.com>
2020-03-18 03:28:52 -0500

    Adding firebase config.

--- src/firebase.js
+++ src/firebase.js
@@ -5,6 +5,14 @@ import 'firebase/auth';
 
 // Your web app's Firebase configuration
 let firebaseConfig = {
+  apiKey: "AIzaSyBFT5yhCTqcjuJB5BgmOcC52vD_gTmcqr4",
+  authDomain: "cv19assist-dev.firebaseapp.com",
+  databaseURL: "https://cv19assist-dev.firebaseio.com",
+  projectId: "cv19assist-dev",
+  storageBucket: "cv19assist-dev.appspot.com",
+  messagingSenderId: "487252002912",
+  appId: "1:487252002912:web:53e28d338ebd8788215b05",
+  measurementId: "G-GB8SL2LGL0"
 };
 
 // Initialize Firebase
