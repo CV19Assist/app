0690f7bc3ba82140771f35274d9bd700ad4e45ab
DavidSheffield <David@KronchUSA.com>
2020-03-17 22:08:38 -0400

    Added Redux Store to Index.js, created reducers.js for combining all reducers in modules

---
+++ src/modules/reducers.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function reducer(state = {state: ''}, action) {
+    switch (action.type) {
+        default:
+            return "No"
+    }
+}
+
+export default reducer
