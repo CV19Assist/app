500f47fc172b7b1b47ea42ada88e990ec7608c9f
jessluo8 <jessie.luo@utdallas.edu>
2020-03-21 01:45:43 -0500

    Removed unused import on user profile page.

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles, colors } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core';
 import Container from '@material-ui/core/Container';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
