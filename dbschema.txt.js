ab76672a15439e70a0c18b9186173bf4cce99da2
Farhan Ahmad <farhan@thebitguru.com>
2020-04-10 12:25:59 -0500

    Adding the original schema file so we can see the proposed changes.

---
+++ dbschema.txt.js
@@ -0,0 +1,114 @@
+import { submitForAssignmentEpic } from "./src/modules/needs";
+
+userProfiles = [
+  {
+    d: {
+      firstName: Joi.string().required(),
+      lastName: Joi.string().required(),
+      displayName: Joi.string().required(),
+      email: Joi.string()
+        .email()
+        .required(),
+      address1: Joi.string().allow(""),
+      address2: Joi.string().allow(""),
+      city: Joi.string().allow(""),
+      state: Joi.string().allow(""),
+      zipcode: Joi.string().allow(""),
+      phone: Joi.string().required(),
+      coordinates: Joi.object().keys({
+        _latitude: Joi.number()
+          .greater(-90)
+          .less(90),
+        _longitude: Joi.number()
+          .greater(-180)
+          .less(180)
+      })
+    }
+  }
+];
+
+needs = [
+  {
+    d: {
+      needs: Joi.array().items(Joi.string()),
+      immediacy: Joi.number()
+        .greater(0)
+        .less(11)
+        .required(),
+        // 1 - not urgent
+        // 5 - not very urgent
+        // 10 - urgent
+
+    
+      // shortDescription: Joi.string().required(),
+      contactInfo: Joi.string().required(),
+      firstName: Joi.string().required(),
+      lastName: Joi.string().required(),
+      otherDetails: Joi.string().allow(""),
+      coordinates: Joi.object().keys({
+        _latitude: Joi.number()
+          .greater(-90)
+          .less(90),
+        _longitude: Joi.number()
+          .greater(-180)
+          .less(180)
+      }),
+
+      // Switching to 1-new-or-released to simplify logic.
+      status: "1-new-or-released 5-released 10-assigned 20-completed",
+
+      createdAt,
+
+      // Optional.  this is set if a user created it.
+      createdBy: { userProfileId, firstName, displayName },
+
+      owner: {
+        userProfileId,
+        firstName,
+        displayName,
+        takenAt
+      },
+
+      // Assignments.  Sub-collection.
+      history: Collection(
+        {
+          action: "1",
+            // 1-created
+            // 5-took-ownership
+            // 10-released
+            // 15-assigned -- for future functionality.
+            // 20-completed
+
+          takenAt, // When the action was taken.
+          userProfileId,
+          firstName,
+          lastName,
+          displayName,
+
+          // if 15-assigned
+          assignedBy
+        }
+      )
+    }
+  }
+];
+
+
+aggregates = {
+  unfulfilledNeedsInfo: [
+    {
+      id: "",
+      createdAt: "",
+      immediacy: "",
+      needs: ["", ""],
+      coordinates: Joi.object().keys({
+        _latitude: Joi.number()
+          .greater(-90)
+          .less(90),
+        _longitude: Joi.number()
+          .greater(-180)
+          .less(180)
+      }),
+    }, ...
+  ]
+};
