9253557475519abc10951647be8b9075a49fe7be
Scott Prue <scott@residebrokerage.com>
2020-04-22 22:47:42 -0700

    Add other events. Add setting of user id.

--- src/components/AnalyticsPageViewLogger/AnalyticsPageViewLogger.js
+++ src/components/AnalyticsPageViewLogger/AnalyticsPageViewLogger.js
@@ -1,16 +1,22 @@
 import { useEffect } from 'react';
-import { useAnalytics } from 'reactfire';
+import { useAnalytics, useUser } from 'reactfire';
 import { useLocation } from 'react-router-dom';
 
 export default function AnalyticsPageViewLogger() {
   const location = useLocation();
   const analytics = useAnalytics();
-  window.analytics = analytics;
-  console.log('page view logger');
+  const user = useUser();
+  // By passing `user.uid` to the second argument of `useEffect`,
+  // we only set user id when it exists
+  useEffect(() => {
+    if (user.uid) {
+      analytics.setUserId(user.uid);
+    }
+  }, [user?.uid]); // eslint-disable-line react-hooks/exhaustive-deps
+
   // By passing `location.pathname` to the second argument of `useEffect`,
   // we only log on first render and when the `pathname` changes
   useEffect(() => {
-    console.log('page view', location.pathname);
     analytics.logEvent('page-view', { path_name: location.pathname });
   }, [location.pathname]); // eslint-disable-line react-hooks/exhaustive-deps
 
