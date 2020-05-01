1a89e5d8252bec76b2ec6fce1e0b20239ee814e2
Scott Prue <sprue.dev@gmail.com>
2020-05-01 19:06:04 -0400

    Add retries. Move eslint to top level

--- functions/src/contactCreated/contactCreated.spec.js
+++ functions/src/contactCreated/contactCreated.spec.js
@@ -19,7 +19,8 @@ describe('contactCreated Firestore Cloud Function (onCreate)', () => {
   });
   // Skipped because test will sometimes timeout within CI as seen
   // here: https://github.com/CV19Assist/app/runs/621415013?check_suite_focus=true#step:8:78
-  it.skip('writes request to mail collection when contact is created', async () => {
+  it('writes request to mail collection when contact is created', async function () {
+    this.retries(3);
     const eventData = { message: '' };
     const userUid = '123ABC';
     // Build onCreate
