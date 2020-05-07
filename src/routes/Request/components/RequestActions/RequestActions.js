eb17c882fb257dc8f2408ed75b6fc32064f9ebea
Scott Prue <sprue.dev@gmail.com>
2020-05-07 19:36:24 -0400

    Fix lint in RequestActions

--- src/routes/Request/components/RequestActions/RequestActions.js
+++ src/routes/Request/components/RequestActions/RequestActions.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import { useFirestore, useUser, useFirestoreDoc } from 'reactfire';
+import { useUser } from 'reactfire';
 import { Alert } from '@material-ui/lab';
 import { LOGIN_PATH } from 'constants/paths';
 import { Button, Grid, Typography, makeStyles } from '@material-ui/core';
